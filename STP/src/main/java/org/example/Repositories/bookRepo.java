20a835cc2403d7df3dfb2e898cd0bfcfeadf8c49
Udius <serga_popov@mail.ru>
2025-01-14 19:52:23 +0500

    .

--- STP/src/main/java/org/example/Repositories/bookRepo.java
+++
@@ -1,7 +0,0 @@
-package org.example.Repositories;
-
-import org.example.Entities.book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface bookRepo extends CrudRepository<book, Long> {
-}
